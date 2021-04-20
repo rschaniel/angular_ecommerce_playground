5586c945a73ad684982ad1a2f18ee6f4c5a6ea55
Ronnie Schaniel <ronnieschaniel@gmail.com>
2021-04-20 20:06:45 +0200

    *resolutions for webpack *second app for orders

---
+++ apps/orders/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'prefetching-sample-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'orders';
+}
